b610ed373c3b7d1d3c595dc181c0125b6844c19c
Alex Olivas <alex.olivas11@gmail.com>
2020-01-09 15:23:16 -0500

    add initial unit tests

--- src/Event.test.js
+++ src/Event.test.js
@@ -1,5 +1,25 @@
 
+import { shallow } from "enzyme";
+import React from "react";
+import Event from "./Event";
+
+// TODO: figure out a way to make this global
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+configure({ adapter: new Adapter() });
+
 describe("Render event tests", () => {
-	test("display event", () => {
+	test("display default event", () => {
+		const wrapper = shallow(<Event>Test</Event>);
+		expect(wrapper.hasClass("event-container")).toEqual(true);
+		expect(wrapper.hasClass("left-event")).toEqual(true);
+		expect(wrapper.hasClass("right-event")).toEqual(false);
+	});
+
+	test("display event oriented to the right", () => {
+		const wrapper = shallow(<Event right>Test</Event>);
+		expect(wrapper.hasClass("event-container")).toEqual(true);
+		expect(wrapper.hasClass("left-event")).toEqual(false);
+		expect(wrapper.hasClass("right-event")).toEqual(true);
 	});
 });
--- src/Timeline.test.js
+++ src/Timeline.test.js
@@ -1,5 +1,16 @@
 
+import { shallow } from "enzyme";
+import React from "react";
+import Timeline from "./Timeline";
+
+// TODO: figure out a way to make this global
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+configure({ adapter: new Adapter() });
+
 describe("Render timeline tests", () => {
 	test("display timeline", () => {
+		const wrapper = shallow(<Timeline>Test</Timeline>);
+		expect(wrapper.hasClass("cronograma-timeline")).toEqual(true);
 	});
 });
