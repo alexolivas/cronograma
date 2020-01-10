147aa26b59dc47e0878324e050b5ef2e137a78e4
Alex Olivas <alex.olivas11@gmail.com>
2020-01-10 16:04:40 -0500

    fixed merge conflicts

--- src/Event.js
+++ src/Event.js
@@ -1,13 +1,13 @@
 import classNames from "classnames";
 import PropTypes from "prop-types";
 import React, { Component } from "react";
-import "./Event.css";
+import "./Event.scss";
 
 export default class Event extends Component {
 	render() {
-		const directionClass = this.props.right ? "right-event" : "left-event";
+		const orientationClass = this.props.right ? "right-event" : "left-event";
 		return (
-			<div className={ classNames("event-container", directionClass) }>
+			<div className={ classNames("event-container", orientationClass) }>
 				<div className="event-body">
 					{ this.props.children }
 				</div>
