a9973c73954ae6b6a3b06effc000417951da8f38
Alex Olivas <alex.olivas11@gmail.com>
2020-01-10 12:44:52 -0500

    jest configuration setup file

--- src/Timeline.test.js
+++ src/Timeline.test.js
@@ -1,12 +1,6 @@
-
 import { shallow } from "enzyme";
 import React from "react";
 import Timeline from "./Timeline";
-
-// TODO: figure out a way to make this global
-import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-configure({ adapter: new Adapter() });
 
 describe("Render timeline tests", () => {
 	test("display timeline", () => {
